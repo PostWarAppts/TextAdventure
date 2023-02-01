f017b02849fd6cef9a03d514a3079d9e161c3742
RD <RD@192.168.2.251>
2023-02-01 01:11:42 -0500

    updated some images as well as started the framework for how the game will work

--- TextAdventure/src/main/Driver.java
+++ TextAdventure/src/main/Driver.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
@@ -27,12 +30,91 @@ public class Driver extends Application {
 		// TODO Auto-generated method stub
 		VBox root = new VBox();
 		
+		Pane p = new Pane();
 		
-		HBox topBar = new HBox();
+		Image image = null;
+		try {
+			image = new Image(new FileInputStream("images/cover2.png"));
+		} catch (Exception exp) {
+		}
+		ImageView view = new ImageView(image);
+		TextField tx = new TextField();
 		
+		tx.setLayoutX(85);
+		tx.setLayoutY(645);
+		tx.setPrefWidth(530);
+		tx.setPrefHeight(50);
+		
+		tx.setStyle("-fx-text-fill: white; -fx-background-color: black");
+		
+		p.getChildren().addAll(view, tx);
+		root.getChildren().add(p);
+		Scene scene = new Scene(root, 700, 900);
+		
+		tx.setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+				if (tx.getText().equals("1") || tx.getText().equals("2") || tx.getText().equals("3") || tx.getText().equals("4") || tx.getText().equals("5") || tx.getText().equals("6")) {
+					root.getChildren().clear();
+					File file = new File(Integer.toString(1) + ".txt");
+					Scanner sc = null;
+					try {
+						sc = new Scanner(file);
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					System.out.println(sc.nextLine());
+					int s = Integer.parseInt(sc.nextLine());
+					
+					ArrayList<Button> topButtons = new ArrayList<>();
+					
+					Pane main = new Pane();
+					
+					main.setPrefSize(700, 850);
+					
+					
+					main.setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000; -fx-font-size: 20; -fx-border-width: 0 4 4 4");
+					
+					
+					mainStuff(topButtons, root);
+					
+					root.getChildren().add(main);
+				} else {
+					
+				}
+			}
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		stage.setTitle("Role-Playing Game");
+		try {
+			stage.getIcons().add(new Image(new FileInputStream("images/circle3.png")));
+		} catch (Exception exp) {
+			
+		}
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+	
+	
+	public static void mainStuff(ArrayList<Button> topButtons, VBox root) {
 		String[] labels = {"Map", "Inventory", "Party", "Status"};
-		
-		ArrayList<Button> topButtons = new ArrayList<>();
+		HBox topBar = new HBox();
 		for (int i = 0; i < 4; i++) {
 			Button b = new Button();
 			b.setPrefSize(175, 50);
@@ -49,77 +131,56 @@ public class Driver extends Application {
 			topBar.getChildren().add(p);
 			topButtons.add(b);
 		}
-		Pane main = new Pane();
 		
-		main.setPrefSize(700, 850);
-		String[] arr = {"Lorum", "Lorum", "Lorum", "Lorum", "Lorum", "Lorum", "Lorum", "Lorum"};
-
-		
-		main.setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000; -fx-font-size: 20; -fx-border-width: 0 4 4 4");
+		root.getChildren().add(topBar);
 		
 		topButtons.get(0).setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000 #000000 #ddcaaa; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
-		mapTab(main, arr, 0);
+
 		
 		topButtons.get(0).setOnAction(e -> {
 			for(Button b : topButtons) {
 				b.setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
 			}
 			topButtons.get(0).setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000 #000000 #ddcaaa; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
-			mapTab(main, arr, 0);
+			
 		});
 		topButtons.get(1).setOnAction(e -> {
 			for(Button b : topButtons) {
 				b.setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
 			}
 			topButtons.get(1).setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000 #000000 #ddcaaa; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
-			inventoryTab(main);
+
 		});
 		topButtons.get(2).setOnAction(e -> {
 			for(Button b : topButtons) {
 				b.setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
 			}
 			topButtons.get(2).setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000 #000000 #ddcaaa; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
-			partyTab(main);
+
 		});
 		topButtons.get(3).setOnAction(e -> {
 			for(Button b : topButtons) {
 				b.setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
 			}
 			topButtons.get(3).setStyle("-fx-background-color: #ddcaaa; -fx-border-color: #000000 #000000 #ddcaaa; -fx-font-size: 20; -fx-border-width: 4 4 4 4");
-			statusTab(main);
 		});
-	
-		
-		root.getChildren().addAll(topBar, main);
 		
 		
-		Scene scene = new Scene(root, 700, 900);
-		stage.setTitle("Role-Playing Game");
-		try {
-			stage.getIcons().add(new Image(new FileInputStream("TextAdventure/images/circle3.png")));
-		} catch (Exception exp) {
-			
-		}
-		stage.setScene(scene);
-		stage.show();
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
+		
+		
 	}
 	
-	public static void mapTab(Pane main, String[] arr, int t) {
+	public static void mapTab(Pane main, String[] arr, int t, int v) {
 		Image image = null;
 		Image image1 = null;
 		main.getChildren().clear();
 		try {
-			image = new Image(new FileInputStream("TextAdventure/images/image.png"));
-			image1 = new Image(new FileInputStream("TextAdventure/images/circle3.png"));
+			image = new Image(new FileInputStream("images/image (2).png"));
+			image1 = new Image(new FileInputStream("images/circle3.png"));
 		} catch (Exception exp) {
 		}
 		ImageView view = new ImageView(image);
-		Rectangle2D rect = new Rectangle2D(0, t, 500, 300);
+		Rectangle2D rect = new Rectangle2D(v, t, 500, 300);
 		view.setViewport(rect);
 		view.setLayoutX(100);
 		view.setLayoutY(50);
@@ -127,8 +188,8 @@ public class Driver extends Application {
 		r.setLayoutX(85);
 		r.setLayoutY(35);
 		ImageView view1 = new ImageView(image1);
-		view1.setLayoutX(340);
-		view1.setLayoutY(200);
+		view1.setLayoutX(146);
+		view1.setLayoutY(96);
 		
 		Rectangle r1 = new Rectangle(530, 230);
 		r1.setLayoutX(85);
@@ -159,8 +220,8 @@ public class Driver extends Application {
 					arr[5] = arr[6];
 					arr[6] = arr[7];
 					arr[7] = txfi.getText();
-					mapTab(main, arr, t+100);
-				} else {
+					mapTab(main, arr, t+55, v);
+				} else if (txfi.getText().equals("E") || txfi.getText().equals("e")) {
 					arr[0] = arr[1];
 					arr[1] = arr[2];
 					arr[2] = arr[3];
@@ -168,7 +229,25 @@ public class Driver extends Application {
 					arr[5] = arr[6];
 					arr[6] = arr[7];
 					arr[7] = txfi.getText();
-					mapTab(main, arr, t);
+					mapTab(main, arr, t, v+55);
+				} else if (txfi.getText().equals("N") || txfi.getText().equals("n")) {
+					arr[0] = arr[1];
+					arr[1] = arr[2];
+					arr[2] = arr[3];
+					arr[4] = arr[5];
+					arr[5] = arr[6];
+					arr[6] = arr[7];
+					arr[7] = txfi.getText();
+					mapTab(main, arr, t-55, v);
+				} else if (txfi.getText().equals("W") || txfi.getText().equals("w")) {
+					arr[0] = arr[1];
+					arr[1] = arr[2];
+					arr[2] = arr[3];
+					arr[4] = arr[5];
+					arr[5] = arr[6];
+					arr[6] = arr[7];
+					arr[7] = txfi.getText();
+					mapTab(main, arr, t, v-55);
 				}
 			}
 		});
